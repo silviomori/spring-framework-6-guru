862ac799e1d1b9f5bcf010bfd7885bd99ee65bb3
Silvio Mori <silviomori@gmail.com>
2023-02-17 15:46:35 -0600

    Create the project for the Book List web app

---
+++ book-list-webapp/src/main/java/com/technomori/guru/booklist/BookListWebappApplication.java
@@ -0,0 +1,13 @@
+package com.technomori.guru.booklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookListWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookListWebappApplication.class, args);
+	}
+
+}
