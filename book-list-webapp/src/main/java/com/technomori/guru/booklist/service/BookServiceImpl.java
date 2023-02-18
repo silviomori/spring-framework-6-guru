e2491f04325b37bf181d7e12b0f4fd33ea9a1da3
Silvio Mori <silviomori@gmail.com>
2023-02-18 12:47:50 -0600

    Refactor: move the BookServiceImpl to the correct package

--- book-list-webapp/src/main/java/com/technomori/guru/booklist/service/BookServiceImpl.java
+++ book-list-webapp/src/main/java/com/technomori/guru/booklist/service/BookServiceImpl.java
@@ -1,11 +1,10 @@
-package com.technomori.guru.booklist;
+package com.technomori.guru.booklist.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.technomori.guru.booklist.domain.Book;
 import com.technomori.guru.booklist.repositories.BookRepository;
-import com.technomori.guru.booklist.service.BookService;
 
 @Service
 public class BookServiceImpl implements BookService {
