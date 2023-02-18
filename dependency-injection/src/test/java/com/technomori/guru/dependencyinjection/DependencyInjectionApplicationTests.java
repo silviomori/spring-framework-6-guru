716e1a55441f010ceb0dc0920be6b4eecfb00132
Silvio Mori <silviomori@gmail.com>
2023-02-18 16:27:25 -0600

    Create project to experiment with Depenency Injection

---
+++ dependency-injection/src/test/java/com/technomori/guru/dependencyinjection/DependencyInjectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technomori.guru.dependencyinjection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
