0f07ed73091df1cee08134ef72780c6ff65f2e85
Silvio Mori <silviomori@gmail.com>
2023-02-19 12:49:05 -0600

    Create the Beer Store project

---
+++ beer-store/src/main/java/com/technomori/guru/beerstore/BeerStoreApplication.java
@@ -0,0 +1,13 @@
+package com.technomori.guru.beerstore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerStoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BeerStoreApplication.class, args);
+	}
+
+}
---
+++ beer-store/src/test/java/com/technomori/guru/beerstore/BeerStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.technomori.guru.beerstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
