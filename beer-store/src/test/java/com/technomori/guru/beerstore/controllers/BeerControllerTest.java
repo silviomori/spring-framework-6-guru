e392633c5dda62f0df3b7d76fa6c1a90fb0c630c
Silvio Mori <silviomori@gmail.com>
2023-02-26 11:17:19 -0600

    Test getBeerById using WebMvcTest, MockMvc, Mockito, Json matchers

--- beer-store/src/test/java/com/technomori/guru/beerstore/controllers/BeerControllerTest.java
+++ beer-store/src/test/java/com/technomori/guru/beerstore/controllers/BeerControllerTest.java
@@ -1,21 +1,59 @@
 package com.technomori.guru.beerstore.controllers;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
 
-import lombok.extern.slf4j.Slf4j;
+import com.technomori.guru.beerstore.domain.Beer;
+import com.technomori.guru.beerstore.services.BeerService;
 
-@SpringBootTest
-@Slf4j
+@WebMvcTest(BeerController.class)
 class BeerControllerTest {
 
     @Autowired
-    BeerController beerController;
+    MockMvc mockMvc;
+
+    @MockBean
+    BeerService beerService;
 
     @Test
-    void testGetBeerById() {
-        log.info(beerController.listBeers().toString());
+    void getBeerById() throws Exception {
+        Beer beerTest = Beer.builder()
+                .id(UUID.randomUUID())
+                .version(1)
+                .beerName("Pliny the Younger")
+                .beerStyle("IPA")
+                .upc("8484957731774")
+                .price(BigDecimal.valueOf(51.37))
+                .quantityOnHand(1122)
+                .createdAt(LocalDateTime.now())
+                .updatedAt(LocalDateTime.now())
+                .build();
+
+        given(beerService.getBeerById(any(String.class))).willReturn(beerTest);
+
+        mockMvc.perform(
+                get("/api/v1/beers/" + beerTest.getId().toString())
+                        .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$.id", is(beerTest.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is(beerTest.getBeerName())));
     }
 
 }
